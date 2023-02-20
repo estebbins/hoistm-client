185e21956a13a002bd839a415b289c5a3afa3984
rectorjordan94 <rectorjordan94@gmail.com>
2023-02-20 09:22:19 -0600

    preliminary styling updates to signUp and signIn pages

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -6,6 +6,7 @@ import messages from '../shared/AutoDismissAlert/messages'
 
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
 
 const SignIn = (props) => {
 	// constructor(props) {
@@ -57,34 +58,43 @@ const SignIn = (props) => {
     return (
         <div className='row'>
             <div className='col-sm-10 col-md-8 mx-auto mt-5'>
-                <h3>Sign In</h3>
-                <Form onSubmit={onSignIn}>
-                    <Form.Group controlId='email'>
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control
-                            required
-                            type='email'
-                            name='email'
-                            value={email}
-                            placeholder='Enter email'
-                            onChange={e => setEmail(e.target.value)}
-                        />
-                    </Form.Group>
-                    <Form.Group controlId='password'>
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                            required
-                            name='password'
-                            value={password}
-                            type='password'
-                            placeholder='Password'
-                            onChange={e => setPassword(e.target.value)}
-                        />
-                    </Form.Group>
-                    <Button variant='dark' type='submit'>
-                        Submit
-                    </Button>
-                </Form>
+                {/* <h3>Sign In</h3> */}
+                <h1 to='/' className='fw-semibold text-center auth-header'>
+				Sign In
+                </h1>
+                <Container className='auth-container'>
+                    <Form onSubmit={onSignIn} className='auth-form'>
+                        <Form.Group controlId='email'>
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control
+                                required
+                                type='email'
+                                name='email'
+                                className='auth-form-control'
+                                value={email}
+                                placeholder='Enter email'
+                                onChange={e => setEmail(e.target.value)}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId='password' className='pt-2'>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                required
+                                name='password'
+                                value={password}
+                                type='password'
+                                placeholder='Password'
+                                className='mb-4 auth-form-control'
+                                onChange={e => setPassword(e.target.value)}
+                            />
+                        </Form.Group>
+                        <Container className='d-flex justify-content-center'>
+                            <Button variant='dark' type='submit' className='auth-submit'>
+                                Submit
+                            </Button>
+                        </Container>
+                    </Form>
+                </Container>
             </div>
         </div>
     )
